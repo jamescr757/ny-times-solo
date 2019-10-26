953ecd3faea06adcfa60b2982cf65977288a2ac4
jamescr757 <55260759+jamescr757@users.noreply.github.com>
2019-10-26 18:44:23 -0500

    api call works. need to work on article style once it's rendered

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,31 @@
 $(document).ready(() => {
 
+    // global api key 
+    const hostEndpoint = 'https:/api.nytimes.com/svc/search/v2/articlesearch.json?';
+    const apiKey = 'api-key=j5A2TiUA2OfdQtG6PM6GZYsUhsBMKy7a';
+    const qSearch = 'q=roosevelt';
+    const fullUrl = `${hostEndpoint}${apiKey}&${qSearch}`;
+    const limit = '5';
 
+    $.ajax({
+        url: fullUrl,
+        method: 'GET'
+    })
+    .then(response => {
+        console.log(response);
 
+        for (let i = 0; i < limit; i++) {
+
+            console.log(response.response.docs[i].byline.original);
+            console.log(response.response.docs[i].headline.main);
+            console.log(response.response.docs[i].web_url);
+
+            $('#article-bin').append(`
+                <
+            `)
+        }
+        
+    })
 
 
 })
