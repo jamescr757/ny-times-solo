63e87ea7775418a1eb0edf75bed78d1b33f4cdb3
jamescr757 <55260759+jamescr757@users.noreply.github.com>
2019-10-27 12:02:30 -0500

    style updated for article bin. looks great. form logic next

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,15 +16,25 @@ $(document).ready(() => {
 
         for (let i = 0; i < limit; i++) {
 
-            console.log(response.response.docs[i].byline.original);
-            console.log(response.response.docs[i].headline.main);
-            console.log(response.response.docs[i].web_url);
+            // console.log(response.response.docs[i].byline.original);
+            // console.log(response.response.docs[i].headline.main);
+            // console.log(response.response.docs[i].web_url);
 
             $('#article-bin').append(`
-                <
-            `)
-        }
-        
+                <blockquote class="blockquote py-3 pl-3">
+                    <span>${i + 1}</span>
+                    <a href=${response.response.docs[i].web_url} class="article-anchor"
+                    target="_blank">
+
+                    <p class="article-title">${response.response.docs[i].headline.main}</p>
+                    
+                    </a>
+                    <footer class="blockquote-footer">${response.response.docs[i].byline.original}</footer>
+                    </blockquote>
+                    `)
+                }
+                
+                // <a href=${response.response.docs[i].web_url} class="article-anchor" target="_blank">Read article</a>
     })
 
 
